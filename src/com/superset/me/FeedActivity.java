1cdb26cb6ada80c82d599303cf2ea87feb8d41dc
roodmichael <mrood@MROODT420S.TheFacebook.com>
2012-07-03 21:19:39 -0700

    Adding in new files from previous commit. These are the new pages.

---
+++ src/com/superset/me/FeedActivity.java
@@ -0,0 +1,14 @@
+package com.superset.me;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FeedActivity extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+        
+        // Grab users feed
+	}
+}
---
+++ src/com/superset/me/FriendsActivity.java
@@ -0,0 +1,14 @@
+package com.superset.me;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FriendsActivity extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+        
+        // Grab users feed
+	}
+}
